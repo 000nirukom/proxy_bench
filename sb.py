90bfa5bf1322714181d21aae5538d5e46b2f2a27
mokurin000 <mokurin000@gmail.com>
2026-01-18 20:57:31 +0800

    feat: add 2022-chacha20 cipher method

--- sb.py
+++ sb.py
@@ -159,6 +159,7 @@ def main():
         ("chacha20-ietf-poly1305",),
         ("2022-blake3-aes-128-gcm",),
         ("2022-blake3-aes-256-gcm",),
+        ("2022-blake3-chacha20-poly1305",),
     ]
 
     results = []
